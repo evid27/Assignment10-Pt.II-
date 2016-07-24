f96a436f4ca2ac39c3ea55c9eb3d80ae99f676d2
dimi88 <kurukii88@gmail.com>
2016-07-24 13:21:11 +0200

    initial sketch

---
+++ assignment10.ino
@@ -0,0 +1,15 @@
+#include <Smartcar.h>
+
+SR04 sensor1, sensor2;
+const int TRIGGER_1 = 6; // Χρησιμοποιήστε τα pin που θέλετε
+const int ECHO_1 = 7; 
+const int TRIGGER_2 = 5;
+const int ECHO_2 = 4;
+
+void setup() {
+  Serial.begin(9600);
+}
+
+void loop() {
+
+}
