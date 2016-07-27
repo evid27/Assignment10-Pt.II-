68af0786d645575166e7a01539e2c593c52cad80
evid27 <evi270103@yahoo.gr>
2016-07-27 11:24:29 +0300

    Update assignment10.ino

--- assignment10.ino
+++ assignment10.ino
@@ -26,17 +26,19 @@ void loop() {
   
   if (leftDistance > 0) {
     leftTOTAL = leftTOTAL + leftDistance;// Αν η αριστερή απόσταση είναι μεγαλύτερη από το 0, πρόσθεσέ την στις τιμές από τον αριστερό αισθητήρα
-    lMeasurements = lMeasurements -1;
+    lMeasurements = lMeasurements -1;//Μείωσε κατά μια τις αριστερές μετρήσεις, μέχρι να γίνουν 10
   }
   if (rightDistance > 0) {
     rightTOTAL = rightTOTAL + rightDistance;//Το ίδιο για τον δεξή αισθητήρα και τις μετρήσεις του
-    rMeasurements = rMeasurements -1;
+    rMeasurements = rMeasurements -1;//Το ίδιο για τις αριστερές μετρήσεις, μέχρι να γίνουν 10
   }
   if (lMeasurements == 0){
-    leftTOTAL = 0;
+    leftTOTAL = 0;//Αν γίνουν 10 μετρήσεις από τον αριστερό αισθητήρα, καθάρισε το συνολικό ποσο των μετρήσεών του
+    lMeasurements = 10;
   }
   if (rMeasurements == 0){
     rightTOTAL = 0;
+    rMeasurements = 10;
   }
   int rAVERAGE = rightTOTAL/10;
   int lAVERAGE = leftTOTAL/10;
